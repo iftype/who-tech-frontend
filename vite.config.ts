9b6f773e895c2a47c7d57c87b7c4b71a3c0a251c
iftype <conditionaltype@gmail.com>
2026-03-26 20:15:18 +0900

    chore: vitest 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   server: {
     port: 5173,
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    include: ['src/**/__tests__/**/*.test.ts'],
   },
 });
